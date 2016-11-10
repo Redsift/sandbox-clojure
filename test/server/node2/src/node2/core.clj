b076ecfc15e55ea7861a1b486bc259ff0ea4a66c
Deepak Prabhakara <deepak@redsift.io>
2016-11-10 16:35:26 +0000

    added local function call to node2/core.clj

--- test/server/node2/src/node2/core.clj
+++ test/server/node2/src/node2/core.clj
@@ -2,5 +2,9 @@
     (:import io.redsift.ComputeRequest
              io.redsift.ComputeResponse))
 
+(defn compute2 []
+  (println "Compute2"))
+
 (defn compute [^ComputeRequest req]
+      (compute2)
       (println "Compute Node2" req))
