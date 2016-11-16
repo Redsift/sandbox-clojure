6a8785ab3c6f798e6f701ccfb6b2dc146428612f
Deepak Prabhakara <deepak@redsift.io>
2016-11-16 14:18:22 +0000

    - expanded node-2.clj with lib imports - added a test to check clojure libs

--- test/server/node-2/project.clj
+++ test/server/node-2/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                           [wit/duckling "0.4.16"]
                            [io.redsift/compute, "1.0"]]
   :repositories {"project" "file:repo"}
-  :main node-2.core)
+  :profiles {:uberjar {:aot [node-2.core]}})
--- test/server/node-2/src/node_2/core.clj
+++ test/server/node-2/src/node_2/core.clj
@@ -1,10 +1,13 @@
 (ns node-2.core
-    (:import io.redsift.ComputeRequest
-             io.redsift.ComputeResponse))
+    (:import io.redsift.ComputeRequest io.redsift.ComputeResponse)
+    (:require clojure.string)
+    (:require [duckling.core :as p]))
 
 (defn compute2 []
   (println "Compute2"))
 
 (defn compute [^ComputeRequest req]
+      (clojure.string/starts-with? "Hello" "Hell")
+      (p/load! {:languages ["en"]})
       (compute2)
       (println "Compute Node2" req))
